a1e1e1895c5cb853eabfc08a142ba4d6d98e716f
alanraso <alanrraso@gmail.com>
2018-09-03 15:10:55 -0300

    Change redis client

--- src/data/db/redis-client.ts
+++ src/data/db/redis-client.ts
@@ -1,8 +1,3 @@
 import * as Redis from 'ioredis';
 
-const redisConfig: Redis.RedisOptions = {
-  host: 'localhost',
-  port: 6379,
-};
-
-export const redisClient = new Redis(redisConfig);
+export const redisClient = new Redis(process.env.REDIS_URL);
