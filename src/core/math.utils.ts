5885d0f2fb4ba291882e1d4471b35c47b5e99bb6
veraldo <everaldogaliano@gmail.com>
2018-12-06 14:55:36 -0200

    Rounding fix

--- src/core/math.utils.ts
+++ src/core/math.utils.ts
@@ -1,6 +1,6 @@
 export const MathUtils = {
   round(n: number, decimalPlaces: number = 0) {
-    const multiplier = decimalPlaces === 0 ? 1 : decimalPlaces * 10;
+    const multiplier = decimalPlaces === 0 ? 1 : 10 ** decimalPlaces;
     return Math.round(n * multiplier) / multiplier;
   },
 };
