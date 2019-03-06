614b7c7d39cf5bea55484b1e25c49f7b5cf783e5
veraldo <everaldogaliano@gmail.com>
2019-03-06 13:01:08 -0300

    payload to push event mapper

--- src/presentation/payload.mapper.ts
+++ src/presentation/payload.mapper.ts
@@ -4,4 +4,14 @@ export const PayloadMapper = {
   mapToDeveloper(payload): DeveloperInput {
     return payload.sender && { developerId: payload.sender.id, name: payload.sender.login };
   },
+  mapToPushEvent(payload) {
+    return {
+      ref: payload.ref,
+      ...payload.head_commit ? {
+        added: payload.head_commit.added,
+        modified: payload.head_commit.modified,
+        removed: payload.head_commit.removed,
+      } : undefined,
+    };
+  },
 };
