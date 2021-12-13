330e8f50cbfa88023195c8a9ccd05aa1d7cd0dae
alanraso <alan.raso@taqtile.com.br>
2021-12-13 10:21:27 -0300

    Upgrade lint and node versions

--- src/test/seed/track.seed.ts
+++ src/test/seed/track.seed.ts
@@ -5,7 +5,6 @@ import { Step, Track } from '@domain/track.model';
 export class TrackSeed {
   tracks: Track[];
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   createTracks(stepsPerTrack: number[]) {
     this.tracks = stepsPerTrack.map((stepsNumber: number, i: number) => {
       return createTrack(i, stepsNumber);
@@ -18,11 +17,11 @@ export class TrackSeed {
 export function createTrack(n: number, stepsNumber = 3) {
   const stepPrefix = `Step ${n} - `;
   const steps: Step[] = new Array(stepsNumber).fill(null).map((_, i: number) => ({
-    body: stepPrefix + i,
+    body: `${stepPrefix}${i}`,
   }));
 
   return {
-    title: 'Title ' + n,
+    title: `Title ${n}`,
     number: n,
     steps,
   };
